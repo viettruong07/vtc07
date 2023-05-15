8f9f41e5b6539f14a9fddc28d482fbfa41b88f79
Trương Công Việt <111971657+viettruong07@users.noreply.github.com>
2023-05-15 11:35:42 +0700

    VietTC

---
+++ employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Employee
+{
+    private string firstName;
+    private string lastName;
+    private string address;
+    private long sin;
+    private double salary;
+
+    public Employee(string firstName, string lastName, string address, long sin, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.sin = sin;
+        this.salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {firstName} {lastName}\nAddress: {address}\nSIN: {sin}\nSalary: {salary:C}";
+    }
+
+    public double CalculateBonus(double percentage)
+    {
+        return salary * percentage / 100.0;
+    }
+}
---
+++ employee2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class employee1
+{
+    public static void Main()
+    {
+        Employee emp = new Employee("VIET", "TRUONG", "37 Nghe An", 0707070707, 7000.0);
+        Console.WriteLine(emp);
+
+        double bonus = emp.CalculateBonus(10.0);
+        Console.WriteLine($"Bonus: {bonus:C}");
+    }
+
+}
