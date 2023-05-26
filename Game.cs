ae262b2bb84210da10e366f92948b6ae2b137d5a
Trương Công Việt <111971657+viettruong07@users.noreply.github.com>
2023-05-26 17:37:32 +0700

    GameItem

---
+++ Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+
+class GameItem
+{
+    public string Name;
+    public string Level;
+    public string Score;
+    private string gun;
+    private string sword;
+    private string bomb;
+
+    public GameItem()
+    {
+    }
+
+    public GameItem(string bomb)
+    {
+        this.bomb = bomb;
+    }
+
+    public GameItem(string gun, string sword)
+    {
+        this.gun = gun;
+        this.sword = sword;
+    }
+
+    public GameItem(string Name, string level, string Score)
+    {
+        this.Name = Name;
+        this.Level = level;
+        this.Score = Score;
+    }
+    public override string ToString() { return Name + "\nName" + Level + "\nLevel" + Score + "\nScere"; }
+
+}
+class Weapon : GameItem
+{
+    public string gun;
+    public string sword;
+
+    public Weapon(string gun, string sword) : base(gun, sword)
+    {
+    }
+
+    public Weapon()
+    {
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+class Armor : GameItem
+{
+    public string Shield;
+    public string cloak;
+
+    public Armor(string Shield, string cloak) : base(Shield, cloak)
+    {
+    }
+
+    public Armor()
+    {
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+class Consumable : GameItem
+{
+    public string bomb;
+
+    public Consumable(string bomb) : base(bomb)
+
+    {
+    }
+
+    public Consumable()
+    {
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ ProgramGame.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        GameItem gameItem = new GameItem();
+        gameItem.Name = "PUBG Mobile";
+        gameItem.Level = "Cao Thu";
+        gameItem.Score = "777";
+
+        Weapon weapon = new Weapon();
+        weapon.sword = "Fire sword";
+        weapon.gun = "AK47 guns";
+
+        Armor armor = new Armor();
+        armor.Shield = "Ice shield";
+        armor.cloak = "Night cloak";
+        Console.WriteLine("Truong Cong Viet Top 1 Sever");
+
+        Consumable comsumable = new Consumable
+        {
+            bomb = "Fire bomb"
+        };
+        Console.WriteLine(gameItem.Name);
+        Console.WriteLine(gameItem.Level);
+        Console.WriteLine(gameItem.Score);
+        Console.WriteLine(weapon.sword);
+        Console.WriteLine(armor.Shield);
+        Console.WriteLine(comsumable.bomb);
+    }
+}
