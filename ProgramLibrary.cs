33541aa9038b7f0dead9b7ff012934bd5c9a93f6
Trương Công Việt <111971657+viettruong07@users.noreply.github.com>
2023-05-26 17:34:59 +0700

    LibraryItem

---
+++ ProgramLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "Developer";
+            book.Author = "Cong Viet";
+            book.PublicationYear = 2021;
+            Console.WriteLine(book.ToString());
+
+
+            Magazine magazine = new Magazine();
+            magazine.Title = "Gia Dinh Minh bat thinh Linh";
+            magazine.Author = "VTV2";
+            magazine.PublicationYear = 2023;
+            Console.WriteLine(magazine.ToString());
+
+            DVD dvd = new DVD();
+            dvd.Title = "Kho qua bo qua?";
+            dvd.Author = "VTC";
+            dvd.PublicationYear = 2020;
+            Console.WriteLine(dvd.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
